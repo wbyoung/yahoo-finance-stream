4f9c190dcd26efffa260bea1b93c6a3c1c0129ae
Whitney Young <wbyoung@fadingred.com>
2015-04-24 17:03:15 -0700

    Increased timeout for test.

--- test/stream_tests.js
+++ test/stream_tests.js
@@ -82,7 +82,7 @@ describe('stream', function() {
     setTimeout(function() {
       stocks.close();
       expect(spy).to.not.have.been.called;
-    }, 1);
+    }, 10);
     stocks.on('data', spy);
     stocks.on('error', done);
     stocks.on('end', function() {
